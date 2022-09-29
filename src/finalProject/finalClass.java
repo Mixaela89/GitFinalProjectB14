31706ece1bf3384368aa6db6160881d13dc61c0e
Mixaela89 <mihela52119@gmail.com>
2022-09-28 20:48:21 -0400

    message2

--- src/finalProject/finalClass.java
+++ src/finalProject/finalClass.java
@@ -3,7 +3,7 @@ package finalProject;
 public class finalClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("this is the code for mercedes"); 
 
 	}
 
